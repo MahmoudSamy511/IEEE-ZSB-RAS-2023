5d6a3d5dbf5293d4efd4bb34899f1cd832d9180a
Mahmoud Samy <mahmoudsamyzx11@gmail.com>
2023-03-10 13:47:04 +0200

    edit2

--- Task1/Assignment 2/Problem.c
+++ Task1/Assignment 2/Problem.c
@@ -2,15 +2,14 @@
 int main(){
     int x =5 , y=6 , z=7;
     int*p=&x ,*q=&y ,*r=&z;
-    int *temp1 ,*temp2;
+    int *temp1 ;
     printf("x= %d\ny= %d\nz= %d\n-------------------\n",x,y,z);
     printf("p= %p\nq= %p\nr= %p\n-------------------\n",p,q,r);
     printf("*p= %d\n*q= %d\n*r= %d\n----------------\n",*p,*q,*r);
     printf("Swapping pointers\n-----------------\n");
     temp1 = p;
-    temp2 = r;
     p= q;
-    q= temp2;
+    q= r;
     r= temp1;
     printf("x= %d\ny= %d\nz= %d\n-------------------\n",x,y,z);
     printf("p= %p\nq= %p\nr= %p\n-------------------\n",p,q,r);
