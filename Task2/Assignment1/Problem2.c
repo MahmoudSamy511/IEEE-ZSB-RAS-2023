110a850dc971671a77633647d4f0c8137cd0b45a
Mahmoud Samy <mahmoudsamyzx11@gmail.com>
2023-03-13 21:11:20 +0200

    Final

--- Task2/Assignment1/Problem2.c
+++ Task2/Assignment1/Problem2.c
@@ -6,7 +6,7 @@ int main(){
     printf("%p\n",ptr);
     printf("%d\n",*ptr++); //use the value of p then moves to next position
     printf("%d\n",*++ptr);  //moves to the next int location then use that value
-    printf("%d\n",++*ptr);  // increments the value by 1 then use it
+    printf("%d\n",++*ptr);  // increments the value by 1 then use it and this lead to #segmentation error  
   
     return 0;
 }
